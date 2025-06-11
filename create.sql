e36102ba0733c1721dc1b9846f17c6041288b5f8
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2025-06-11 08:58:08 +0100

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS ex07a (
+CREATE TABLE IF NOT EXISTS ex06a (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	text VARCHAR(30) NOT NULL,
 	num FLOAT NOT NULL
 );
 
-INSERT INTO ex07a VALUES
+INSERT INTO ex06a VALUES
 	(1, "Hello World!!", 2.21),
 	(2, "Lorem ipsum, dolor sic amet", 7.56),
 	(3, "Sample text", 6.00);
